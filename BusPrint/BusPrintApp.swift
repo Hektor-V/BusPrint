daaba0f5420bccc7da1eb1c33439f8c6954243f1
Hector <hectorgvazquez@gmail.com>
2021-12-29 16:00:04 -0600

    Initial Commit

---
+++ BusPrint/BusPrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusPrintApp.swift
+//  BusPrint
+//
+//  Created by Hector Vazquez on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BusPrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
