34800f5726724fd5ab6d942624bf84a250597508
Hector <hectorgvazquez@gmail.com>
2021-12-29 22:12:48 -0600

    First commit and push. This includes pod file and pod file lock for firebase, also includes plist.

--- BusPrint/BusPrintApp.swift
+++ BusPrint/BusPrintApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseCore
 
 @main
 struct BusPrintApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
