1ed5abb3f30637c2374913e431f3a5ce304b8591
Vladimir Nava <72510059+vnava98@users.noreply.github.com>
2022-04-29 19:33:22 -0500

    Update SettingsViewV2.swift

--- BusPrint/Features/Settings/SettingsViewV2.swift
+++ BusPrint/Features/Settings/SettingsViewV2.swift
@@ -6,46 +6,85 @@
 //
 import SwiftUI
 
+//import Firebase
+
+// TODO: Change appearance to match overall app design
+// TODO: Add functionality to EmailView and PasswordView
 struct SettingsView: View {
-    var body: some View{
+    // vars for DarkMode
+    @State var isDarkModeOn: Bool = false
+
+    var body: some View {
+        // NavigationView for the settings page
         NavigationView {
-            
-            Form{
-                
-                Section(header: Text("User")) {
-                    
-                    Toggle(isOn:.constant(true)){
+            // Form contains sections for each feature
+            Form {
+                Section("Appearance") {
+                    // TODO: remember users selection
+                    Toggle(isOn: $isDarkModeOn) {
                         Text("Dark Mode")
                     }
+                }
+                
+                Section("Account") {
+                    // Email option
+                    NavigationLink(destination: EmailView(), label: {
+                        SettingsRowView(title: "Email", systemImageName: "envelope")
+                    })
                     
-                }
-                
-                Section(header: Text("Privacy")){
+                    // Password option
+                    NavigationLink(destination: PasswordView(), label: {
+                        SettingsRowView(title: "Password", systemImageName: "ellipsis.rectangle")
+                    })
                     
-                    Text("Privacy Policy")
-                }
-                
-                Section(header: Text("Account")){
-                    Text("Email")
-                    Text("Password")
-                    Text("Notifications")
-                }
-                
-                
-                Button("Sign Out"){
-                    print("Signed Out")
+                    // Notify option
+                    NavigationLink(destination: NotifyView(), label: {
+                        SettingsRowView(title: "Notifications", systemImageName: "phone")
+                    })
+                    
                 }
 
                 
+                Section("About") {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("v.1.0")
+                    }
+                }
+                
+                Button("Sign Out", role: .destructive){
+                    // sign the user out of firebase
+                    /*
+                     let firebaseAuth = Auth.auth()
+                     do {
+                     try firebaseAuth.signOut()
+                     } catch let signOutError as NSError {
+                     print("Error signing out: %@", signOutError)
+                     */
+                }
             }
-            .navigationTitle("Settings")
+            .navigationBarTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
-    
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+    }
+}
+
+// struct handling the settings row views
+// 2 vars: title and systemimage (SF Symbols)
+struct SettingsRowView: View {
+    var title : String
+    var systemImageName : String
+    var body : some View {
+        HStack (spacing : 15) {
+            Image(systemName: systemImageName)
+            Text(title)
+        }
     }
 }
